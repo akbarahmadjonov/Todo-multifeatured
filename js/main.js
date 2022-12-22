35240b48b3e3aace1ef213957448a799f2cfa911
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-12-22 16:15:38 +0500

    Fix bug

--- js/main.js
+++ js/main.js
@@ -77,14 +77,13 @@ elForm.addEventListener("submit", function (evt) {
   elInput.value = "";
 
   //*----------------  Localstorage
-//   localStorage.setItem("user", JSON.stringify(todos));
-//   localStorage.getItem("user");
-//   console.log(todos);
+  localStorage.setItem("user", JSON.stringify(todos));
+  localStorage.getItem("user");
 });
 
 //*---------------- Localstorage
-// todos = JSON.parse(localStorage.getItem("user"));
-// renderTodo(todos, elList);
+todos = JSON.parse(localStorage.getItem("user"));
+renderTodo(todos, elList);
 //*---------------- Localstorage
 
 elList.addEventListener("click", function (evt) {
